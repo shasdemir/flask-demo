776add5a7ccd368862304e5aaf3e70730abf8250
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-10-03 21:15:32 +0300

    Added stylesheet.

--- flask_demo.py
+++ flask_demo.py
@@ -48,7 +48,7 @@ def show_entries():
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)'), [request.form['title'], request.form['text']]
+    g.db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
     g.db.commit()
     flash('New entry was successfully posted.')
     return redirect(url_for('show_entries'))
