e95a9bf517fd97a0f3c5175161c79168e389c3ef
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-10-03 20:02:22 +0300

    Login / logout.

--- flask_demo.py
+++ flask_demo.py
@@ -46,11 +46,34 @@ def show_entries():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if nit session.get('logged_in'):
+    if not session.get('logged_in'):
         abort(401)
     g.db.execute('insert into entries (title, text) values (?, ?)'), [request.form['title'], request.form['text']]
     g.db.commit()
     flash('New entry was successfully posted.')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username.'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in!')
+            return redirect(url_for('show_entries'))
+    else:
+        return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
     return redirect(url_for('show_entries'))
 
 
